6a803675483c28f0a97c765cf5ed7d3880eac229
suanjin <108645919+suanjin@users.noreply.github.com>
2022-10-09 18:01:19 +0800

    Update util.js

--- main/JS/lib/util.js
+++ main/JS/lib/util.js
@@ -1,8 +1,8 @@
-import 'assets://js/lib/uri.min.js'
-import cheerio from 'assets://js/lib/cheerio.min.js';
-import 'assets://js/lib/crypto-js.js'
-import 'assets://js/lib/dayjs.min.js'
-import _ from 'assets://js/lib/underscore-esm-min.js'
+import 'assets://JS/lib/uri.min.js'
+import cheerio from 'assets://JS/lib/cheerio.min.js';
+import 'assets://JS/lib/crypto-js.js'
+import 'assets://JS/lib/dayjs.min.js'
+import _ from 'assets://JS/lib/underscore-esm-min.js'
 
 var charStr = 'abacdefghjklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789';
 export function randIndex(min, max, i) {
