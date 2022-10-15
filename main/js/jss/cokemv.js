a79bed8062182384e414c9e64042bafb591eddea
suanjin <108645919+suanjin@users.noreply.github.com>
2022-10-15 16:57:20 +0800

    Update cokemv.js

--- main/js/jss/cokemv.js
+++ main/js/jss/cokemv.js
@@ -1,5 +1,6 @@
 var rule = Object.assign(muban.mxpro,{
 title:'cokemv',
 host:'https://cokemv.me',
+url:'/index.php/vod/show/id/fypage.html',
 class_parse:'.navbar-items li:gt(1):lt(7);a&&Text;a&&href;/(\\d+).html',
 });
