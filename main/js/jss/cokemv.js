931db197ca37df36c9091423d693b06c5fc8c10f
suanjin <108645919+suanjin@users.noreply.github.com>
2022-10-17 11:15:56 +0800

    Delete main/js directory

--- main/js/jss/cokemv.js
+++
@@ -1,6 +0,0 @@
-var rule = Object.assign(muban.mxpro,{
-title:'cokemv',
-host:'https://cokemv.me',
-url:'/index.php/vod/show/id/fypage.html',
-class_parse:'.navbar-items li:gt(1):lt(7);a&&Text;a&&href;/(\\d+).html',
-});
